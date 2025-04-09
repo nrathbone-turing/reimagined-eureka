90de547b10d3bb21654fc2ae164aff73cd97bd23
Nick Rathbone <nrathbone@joinhandshake.com>
2025-04-09 10:32:05 -0600

    Add course class and tests for functionality to enroll students and check if the course is full or not

---
+++ lib/course.rb
@@ -0,0 +1,23 @@
+class Course
+
+  attr_reader :name, :capacity, :students, :full
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @students = []
+    @full = false
+  end
+
+
+
+
+  def enroll(student)
+    @students << student
+  end
+
+  def full?
+    @students.size >= @capacity
+  end
+  
+end
--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -1,43 +1,53 @@
+require 'rspec'
+require 'pry'
+require './lib/student'
+require './lib/course'
+
+RSpec.describe Course do
+
+  before(:each) do
+    @course = Course.new("Calculus", 2)
+  end
+
+  it 'exists' do
+  expect(@course).to be_an_instance_of(Course)
+  end
+
+  it 'has a name of Calculus' do
+    expect(@course.name).to eq("Calculus")
+  end
+
+  it 'has a capacity of 2' do
+    expect(@course.capacity).to eq(2)
+  end
+
+  it 'has an empty array of students' do
+    expect(@course.students).to eq([])
+  end
+
+  it 'is not full with no students' do
+    expect(@course.full).to eq(false)
+  end
+
+  it 'can enroll students successfully' do
+    student1 = Student.new({name: "Morgan", age: 21})
+    student2 = Student.new({name: "Jordan", age: 29})
+    
+    @course.enroll(student1)
+    @course.enroll(student2)
+
+    expect(@course.students). to eq([student1, student2])
+  end
+
+  it 'is full when the number of students is >= the capacity' do
+    student1 = Student.new({name: "Morgan", age: 21})
+    student2 = Student.new({name: "Jordan", age: 29})
+    
+    @course.enroll(student1)
+    @course.enroll(student2)
+
+    expect(@course.full?).to eq(true)
+  end
 
 
-
-
-
-
-# pry(main)> require './lib/course'
-# # => true
-
-# pry(main)> require './lib/student'
-# # => true
-
-# pry(main)> course = Course.new("Calculus", 2)    
-# # => #<Course:0x00007fa0a69be328...>
-
-# pry(main)> course.name
-# # => "Calculus"
-
-# pry(main)> course.capacity
-# # => 2
-
-# pry(main)> course.students
-# # => []
-
-# pry(main)> course.full?
-# # => false
-
-# pry(main)> student1 = Student.new({name: "Morgan", age: 21})
-# # => #<Student:0x00007fa0a80ae588...>
-
-# pry(main)> student2 = Student.new({name: "Jordan", age: 29})    
-# # => #<Student:0x00007fa0a814f4d8...>
-
-# pry(main)> course.enroll(student1)    
-
-# pry(main)> course.enroll(student2)    
-
-# pry(main)> course.students
-# # => [#<Student:0x00007fa0a80ae588...>, #<Student:0x00007fa0a814f4d8...>]
-
-# pry(main)> course.full?
-# # => true
-
+end
